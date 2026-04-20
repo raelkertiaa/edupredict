5f40ec2f49d50d463dbc136e2c2b07cfdc21d265
Nur Ayu Aini Amalia <ayuainiamalia26@gmail.com>
2026-04-20 18:51:50 +0700

    mengganti bg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-slate-900 flex items-center justify-center font-sans text-slate-200">
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center font-sans text-slate-200">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-blue-400 mb-4">
           EduPredict
